42cdc6fa93198ed0259ef5d3e29083121f8d8795
twong5200 <twong5200@gmail.com>
2025-03-07 16:42:34 +0000

    fix#1

--- authConfig.js
+++ authConfig.js
@@ -6,8 +6,8 @@
 require('dotenv').config();
 
 const TENANT_SUBDOMAIN = process.env.TENANT_SUBDOMAIN || 'unfcuappsInfraPOC';
-const REDIRECT_URI = process.env.REDIRECT_URI || 'http://localhost:3000/auth/redirect';
-const POST_LOGOUT_REDIRECT_URI = process.env.POST_LOGOUT_REDIRECT_URI || 'http://localhost:3000';
+const REDIRECT_URI = process.env.REDIRECT_URI || 'https://unfcudemo-fcg3bga8c5ejd3de.eastus2-01.azurewebsites.net/auth/redirect';
+const POST_LOGOUT_REDIRECT_URI = process.env.POST_LOGOUT_REDIRECT_URI || 'https://unfcudemo-fcg3bga8c5ejd3de.eastus2-01.azurewebsites.net';
 
 /**
  * Configuration object to be passed to MSAL instance on creation.
@@ -17,7 +17,7 @@ const POST_LOGOUT_REDIRECT_URI = process.env.POST_LOGOUT_REDIRECT_URI || 'http:/
 const msalConfig = {
     auth: {
         clientId: process.env.CLIENT_ID || '93415181-5d5a-426d-b50c-07ed37833261', // 'Application (client) ID' of app registration in Microsoft Entra admin center - this value is a GUID
-        authority: process.env.AUTHORITY || `https://${TENANT_SUBDOMAIN}.ciamlogin.com/`, // Replace the placeholder with your tenant name
+        authority: process.env.AUTHORITY || `https://unfcuappsInfraPOC.ciamlogin.com/`, // Replace the placeholder with your tenant name
         clientSecret: process.env.CLIENT_SECRET || 'BlL8Q~BI1Lg40ta-ee3g9raLbcvRoOMOwaFh1bXr', // Client secret generated from the app registration in Microsoft Entra admin center
     },
     system: {
