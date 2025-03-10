9934c9ecd91ac7260cf5a1fb41100d43912d11a5
twong5200 <twong5200@gmail.com>
2025-03-10 01:16:54 +0000

    update#6

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,37 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function (req, res, next) {
-    res.render('index', {
-        title: 'UNFCU External ID Demo',
-        isAuthenticated: req.session.isAuthenticated,
-        username: req.session.account?.username !== '' ? req.session.account?.username : req.session.account?.name,
-    });
-});
+// custom middleware to check auth state
+function isAuthenticated(req, res, next) {
+    if (!req.session.isAuthenticated) {
+        return res.redirect('/auth/signin'); // redirect to sign-in route
+    }
+    next();
+};
+
+router.get('/id',
+    isAuthenticated, // check if user is authenticated
+    async function (req, res, next) {
+        // Check if roles exist in idTokenClaims and determine user type
+        const roles = req.session.account.idTokenClaims?.roles || [];
+        const isMsr = Array.isArray(roles) && roles.includes('msr');
+        const isMsr2 = Array.isArray(roles) && roles.includes('msrlevel2');
+        const isMember = Array.isArray(roles) && roles.includes('member');
+        const name = req.session.account?.name;
+
+        res.render('id', { 
+            idTokenClaims: req.session.account.idTokenClaims,
+            isMsr: isMsr,
+            isMember: isMember,
+            isMsr2: isMsr2,
+            name: name
+        });
+    }
+);
 
 module.exports = router;
